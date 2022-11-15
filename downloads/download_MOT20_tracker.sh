75a783e10d3c796f7f660a85f5a544c1dbd546a1
dendorferpatrick <patrick.dendorfer@tum.de>
2022-11-15 10:53:23 +0100

    changes server

--- downloads/download_MOT20_tracker.sh
+++ downloads/download_MOT20_tracker.sh
@@ -2,3 +2,4 @@
 
 wget https://vision.in.tum.de/webshare/g/papers/dendorfer/quovadis/MOT20_tracker.zip
 unzip MOT20_tracker.zip
+rm MOT20_tracker.zip
--- downloads/download_pretrained_predictors.sh
+++ downloads/download_pretrained_predictors.sh
@@ -2,3 +2,4 @@
 
 wget https://vision.in.tum.de/webshare/g/papers/dendorfer/quovadis/predictor_weights.zip
 unzip predictor_weights
+rm predictor_weights.zip
